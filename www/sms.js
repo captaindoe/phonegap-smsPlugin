4427baad298c3f330bf5ec00498e03632418db90
David Wibergh <davidwibergh@c-178-73-200-2.anonymous.at.anonine.com>
2013-09-23 13:53:59 +0200

    Första commit

---
+++ www/sms.js
@@ -0,0 +1,13 @@
+var sms = {
+    send: function(phone, message, method, successCallback, failureCallback) {
+        cordova.exec(
+            successCallback,
+            failureCallback,
+            'Sms',
+            'send',
+            [phone, message, method]
+        );
+    }
+}
+
+module.exports = sms;
